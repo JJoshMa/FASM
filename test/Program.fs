711bf2f255921a4bc9a73601bebec96ed99ded8e
wang <3068629642@qq.com>
2024-09-29 18:46:41 +0800

    完善FileHeader

--- test/Program.fs
+++ test/Program.fs
@@ -2,8 +2,20 @@
 
 module test = 
     open Elf
-    
-    let run () = 
-        Say.hello "world"
+    open System.IO
 
-test.run()
+    let writeInt16ToFile (filePath: string) (value: int16) =
+        // 使用 FileStream 和 BinaryWriter 来写入 int16 值
+        use fs = new FileStream(filePath, FileMode.Create, FileAccess.Write)
+        use writer = new BinaryWriter(fs)
+        
+        writer.Write(value)
+
+let main () =
+    let filePath = "output.bin"  // 文件路径
+    let valueToWrite  = int16 12345  // 要写入的 int16 值
+    
+    test.writeInt16ToFile filePath valueToWrite
+    printfn "Successfully wrote %d to %s" valueToWrite filePath
+
+main ()
